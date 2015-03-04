45b7a0fd0874a4c76992e73b626299033f6a2d7b
codesRus <rchemelekov@gmail.com>
2015-03-03 20:20:34 -0800

    Added ActionBar menu items. Currently the only thing they do when pressed is display a "Toast" but feel free to remove any (or all) if you don't like the idea. If the little icons don't upload along with this, let me know and I'll post them (You'll know if they didn't upload if Android Studio marks them red.) manually.
    Made onClick a bit more flexible to avoid having to manually change things if we decided to change the text on any of the buttons.
    
    Added a Test activity to the ActionBar so you guys would get an idea of how the LocationActivity and YelpActivity that Kevin implemented work. (Note: results are displayed in the command line.)
    I'm thinking they should be Services instead of Activities, so maybe I'll convert them over, unless anyone has a better idea?

---
+++ app/src/main/java/com/csm117/foodapp/TEST.java
@@ -0,0 +1,70 @@
+package com.csm117.foodapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class TEST extends ActionBarActivity implements View.OnClickListener {
+
+    Button btnGetLocation;
+    Button btnRunQuery;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        btnGetLocation = (Button) findViewById(R.id.btnLocation);
+        btnRunQuery = (Button) findViewById(R.id.btnQuery);
+
+        btnGetLocation.setOnClickListener(this);
+        btnRunQuery.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+
+        switch (v.getId()) {
+            case R.id.btnLocation:
+                Toast.makeText(this, "Locating...", Toast.LENGTH_SHORT).show();
+                intent = new Intent(TEST.this, LocationActivity.class);
+                startActivity(intent);
+                return;
+            case R.id.btnQuery:
+                Toast.makeText(this, "Querying...", Toast.LENGTH_SHORT).show();
+                intent = new Intent(TEST.this, YelpActivity.class);
+                startActivity(intent);
+                return;
+            default:
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_test, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
