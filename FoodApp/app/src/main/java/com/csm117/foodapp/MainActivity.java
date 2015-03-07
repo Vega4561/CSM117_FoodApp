14dfb8b52e137f32b18bd279c301b1a3c1c6952e
Vega4561 <josealex380@gmail.com>
2015-03-07 15:48:17 -0800

    Deleted some clutter
    I guess that last push WAS an update.

--- FoodApp/app/src/main/java/com/csm117/foodapp/MainActivity.java
+++ FoodApp/app/src/main/java/com/csm117/foodapp/MainActivity.java
@@ -33,13 +33,6 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     Button acceptButton;
     Button callButton;
 
-    // For test
-    JSONObject steak_bowl = new JSONObject();
-    JSONObject sushi = new JSONObject();
-    JSONObject blank = new JSONObject();
-    JSONArray steakImgs = new JSONArray();
-    JSONArray sushiImgs = new JSONArray();
-    JSONArray blankImgs = new JSONArray();
 
     JSONArray GalImages;
     //JSONArray TestImages;
@@ -84,62 +77,13 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         PorterDuffColorFilter filterDc = new PorterDuffColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
         dc.setColorFilter(filterDc);
         callButton.setTextColor(Color.parseColor("white"));
-        // Test data setup
-        /*
-        steakImgs.put("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBKJ2dWOpjxA4q-yZHulrmqWLWkceCcOyrwxSDCNAF54UsYF_zug");
-        steakImgs.put("https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyApzOGdZ7s7U-BMdc90aZqMnwrZWxctMwU&photoreference=CnRnAAAA_10uiYflyBTq4vkcouxtEFBgEs_DU" +
-                "lneTogs1cjnA9SWajnLaNErpHEjIOV9IpBE2ZbJ_4ppIvJuUFDi8IW5D9S7AdpeSUYJLHsqV24oHOpIU1DEGecur9PBOWdEUxIAMYVicl5Gx364CC7htC_rLBIQhKfbm0LMPRnaFdAx2WZKZRoUslNK2" +
-                "FWctkdw-npqJ-joJdBtMnM&maxwidth=200");
-        steakImgs.put("http://s3-media4.fl.yelpcdn.com/bphoto/-Qt7jSghOrJu9rRTOPNb9w/348s.jpg");
-        sushiImgs.put("http://s3-media3.fl.yelpcdn.com/bphoto/1XoBXYs2X0MqtWzpsN3HDg/l.jpg");
-        sushiImgs.put("http://unvegan.com/wp-content/uploads/2010/01/IMG_2784.jpg");
-        //blankImgs.put("http://img2.wikia.nocookie.net/__cb20130511180903/legendmarielu/images/b/b4/No_image_available.jpg");
 
-
-        try {
-            steak_bowl.put("photoUrls", steakImgs);
-           // steak_bowl.put("dish_name", "Teriyaki steak bowl");
-            steak_bowl.put("name", "Gushi");
-            steak_bowl.put("url", "http://gushi.menu");
-            steak_bowl.put("phone", "310-208-4038");
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        try {
-            sushi.put("photoUrls", sushiImgs);
-            //sushi.put("dish_name", "Sandra Roll");
-            sushi.put("name", "Yamato");
-            sushi.put("url", "http://www.yelp.com/biz/yamato-restaurant-los-angeles");
-            sushi.put("phone", "310-208-0100");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        try {
-            blank.put("photoUrls", blankImgs);
-            //blank.put("dish_name", "Blank entry (test)");
-            blank.put("name", "Blank restaurant (test)");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }/*
-        GalImages = new JSONArray();
-        GalImages.put(steak_bowl);
-        GalImages.put(sushi);
-        GalImages.put(blank);*/
-        //JSONArray jsonImages = new JSONArray();
         try {
             GalImages = new JSONArray(placesQueryJSON);
         }
         catch (Throwable t){
             Log.e("Food App", "Could not parse malformed JSON: \"" + placesQueryJSON + "\"");
-        }/*
-        if(jsonImages.optJSONObject(0).has("photoUrls") && jsonImages.optJSONObject(0).optJSONArray("photoUrls") != null){
-        Log.d("Food app", String.valueOf(jsonImages.optJSONObject(0).optJSONArray("photoUrls").length()));}
-        if(jsonImages.optJSONObject(1).has("photoUrls") && jsonImages.optJSONObject(1).optJSONArray("photoUrls") != null){
-            Log.d("Food app", String.valueOf(jsonImages.optJSONObject(1).optJSONArray("photoUrls").length()));}
-        if(jsonImages.optJSONObject(2).has("photoUrls") && jsonImages.optJSONObject(2).optJSONArray("photoUrls") != null){
-            Log.d("Food app", String.valueOf(jsonImages.optJSONObject(2).optJSONArray("photoUrls").length()));}
-*/
+        }
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
 
 
